86e656c147ef125ffa3167c14114d790a26ddd37
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-01 14:37:07 -0300

    -

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root 'demo#index'
+  get 'demo/index'
+  get 'demo/hello'
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  
+  #default route structure
+  #controller - action - id 
+  #get ':controller(/:action(/:id))'
+  #match ':controller(/:action(/:id))', :via => :get
+
+  
 end
