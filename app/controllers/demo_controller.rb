35850b73521861418b4af540ce121864ee49ccdf
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-01 18:47:52 -0300

    testing rails

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,8 +1,25 @@
 class DemoController < ApplicationController
   
+  layout false
   def index
+    #render("demo/hello")
   end
 
   def hello
+    @target = "word"
+
+    @id = params["id"]
+    @page = params[:page]
+    # render :json => {"hello": true}
+    # redirect_to(:controller => 'demo', :action => 'index')
+    # redirect_to(:action => 'index') #is the same controller
+  end
+
+  def about
+    render("about_us")
+  end
+
+  def contact
+    render("contact_us")
   end
 end
