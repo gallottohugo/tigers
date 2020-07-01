52bb0cf1a843137d9ff9be9b73b6506345e19542
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-01 18:48:35 -0300

    Testing rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  root 'demo#index'
-  get 'demo/index'
-  get 'demo/hello'
+  #root 'demo#index'
+  
+  
   devise_for :users, controllers: { sessions: 'users/sessions' }
 
-
+  # get 'demo/hello'
+  # get 'demo/index'
+  get ':controller(/:action(/:id))'
   
-  #default route structure
-  #controller - action - id 
-  #get ':controller(/:action(/:id))'
-  #match ':controller(/:action(/:id))', :via => :get
+  # default route structure
+  # controller - action - id 
+  # match ':controller(/:action(/:id))', :via => :get
 
   
 end
