d28815abf19bb2805607720e45b26ba3f9ac7aaf
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-02 12:46:06 -0300

    Refact user registration controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -10,9 +11,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    user = User.create(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
+    render :json => user
+  end
 
   # GET /resource/edit
   # def edit
