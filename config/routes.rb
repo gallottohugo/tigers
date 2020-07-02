81a66d95f6a90bc1d16130c6f98660d02a02b532
Hugo Andrés Gallotto <gallottohugo@gmail.com>
2020-07-02 12:45:48 -0300

    Refact routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   #root 'demo#index'
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-  devise_for :customers
-  devise_for :employees
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :customers, controllers: { sessions: 'customers/sessions', registrations: 'customers/registrations' }
+  devise_for :employees, controllers: { sessions: 'employees/sessions', registrations: 'employees/registrations' }
   
   resources :guards do 
     member do 
